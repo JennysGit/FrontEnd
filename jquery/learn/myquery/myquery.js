e3b4623ae5c9230a7f8bff75552aaae47713f8c1
Jing Zhang <jing3.zhang@symbio.com>
2016-12-07 18:12:00 +0800

    jquery

--- jquery/learn/myquery/myquery.js
+++ jquery/learn/myquery/myquery.js
@@ -1,3 +1,4 @@
+//立即执行函数，通过闭包创建私有私有变量
 (function( global, factory ){
 
 }(typeof window !== "undefined" ? window : this, function(window ngGlobal){
@@ -5,4 +6,55 @@
 		return new jQuery.fn.init( selector, context );
 	};
 
-}))
+}));
+
+(function(global, factory){
+	// "use strict";
+	// if( typeof module === "object" && typeof module.exports === ""){
+
+	// }
+
+	factory( global );
+
+})
+// 立即执行函数的参数, global,回调函数
+(
+window, function(window, ngGlobal){
+	// main 主体
+	"use strict";
+
+	var arr = [];
+	var document = window.document;
+
+	// 返回指定对象的原型。
+	var getProto = Object.getPrototypeOf;
+
+	// slice 
+	var slice = arr.slice;
+
+	// concat
+	var concat = arr.concat;
+
+	//  push
+	var push = arr.pushl
+
+	// indexOf
+	var indexOf = arr.indexOf;
+
+	class2type = {};
+
+	var toString = class2type.toString;
+
+	hasOwn = class2type.hasOwnProperty;
+
+	var fnToString = hasOwn.toString;
+
+	var ObjectFunctionString = fnToString.call( Object );
+
+	var support = {};
+
+	function DOMEval(code, doc){
+		
+	}
+
+});
